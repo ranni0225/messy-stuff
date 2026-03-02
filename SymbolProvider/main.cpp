273ea517b6ffc26491dfa930c0f292de1c51a348
ranni <101507816+ranni0225@users.noreply.github.com>
2026-03-02 08:50:30 +0800

    feat(SymbolProvider): -

---
+++ SymbolProvider/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <Windows.h>
+
+#pragma optimize("", off)
+
+template<typename T>
+void ProvideSymbol()
+{
+    T instance{};
+}
+
+__declspec(dllexport) void ProvideSymbolsHere()
+{
+    // .reload /f /v sp.dll=0x7FFFF0000000
+
+    struct TestType
+    {
+        int i;
+    };
+
+    ProvideSymbol<TestType>();
+}
+
+#pragma optimize("", on)
+
+BOOL WINAPI DllMain(HINSTANCE, DWORD, LPVOID)
+{
+    return TRUE;
+}
